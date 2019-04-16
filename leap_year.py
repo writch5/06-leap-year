3f93601a4a222af659c9080bdbaedfb7a6bf20ae
Will Ritchie <49207650+writch5@users.noreply.github.com>
2019-04-15 19:47:43 -0600

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -16,15 +16,3 @@ def is_leap(year):
     return flag
     
 
-# Below is a set of tests so you can check if your code is correct.
-from test import testEqual
-
-testEqual(is_leap(1944), True)
-testEqual(is_leap(2011), False)
-testEqual(is_leap(1986), False)
-testEqual(is_leap(1956), True)
-testEqual(is_leap(1957), False)
-testEqual(is_leap(1800), False)
-testEqual(is_leap(1900), False)
-testEqual(is_leap(1600), True)
-testEqual(is_leap(2056), True)

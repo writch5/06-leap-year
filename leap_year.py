23d7ca06c1d2420019cace7478eb4d8f638ce5c9
Will Ritchie <49207650+writch5@users.noreply.github.com>
2019-04-15 20:01:38 -0600

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,18 +1,8 @@
-def is_leap(year):
-    # your code here
-    
-    flag = False
-    
-    if year % 4 == 0:
-       flag = True
-        
-    if year % 100 == 0:
-       flag = False
-            
-    if year % 400 == 0:
-       flag = True
-    
-    
-    return flag
-    
+# Write a function `is_leap_year` that takes a year as a parameter
+#   -->**RETURNS**<-- True if the year is a leap year, False otherwise.
+# The logic-chain is somewhat similar to the Sherlock problem.
 
+# Don't forget to reach out for help after your own due diligence
+
+def is_leap_year(year):
+    return ''
